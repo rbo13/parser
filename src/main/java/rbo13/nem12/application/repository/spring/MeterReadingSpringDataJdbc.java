0359fcd41d80ee6ee82bae420c5431ed740c8742
rbo13 <chardyy.orofeo@gmail.com>
2024-09-12 12:46:20 +0800

    chore: cleaning up on unused code
    Signed-off-by: rbo13 <chardyy.orofeo@gmail.com>

--- src/main/java/rbo13/nem12/application/repository/spring/MeterReadingSpringDataJdbc.java
+++
@@ -1,9 +0,0 @@
-package rbo13.nem12.application.repository.spring;
-
-import org.springframework.data.repository.CrudRepository;
-import rbo13.nem12.application.model.MeterReading;
-
-import java.util.UUID;
-
-public interface MeterReadingSpringDataJdbc extends CrudRepository<MeterReading, UUID> {
-}
